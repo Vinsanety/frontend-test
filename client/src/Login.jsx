0186d0f8d43745d2a7be1067bc00f39ac5be9fb9
vince vahala <vincevahala@gmail.com>
2019-04-05 11:23:40 -0600

    Form state, minor css

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,35 +1,42 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
   constructor() {
     super();
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      name: '',
+      password: '',
+    }
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    const data = new FormData(event.target);
-
-    fetch('/api/form-submit-url', {
-      method: 'POST',
-      body: data,
-    });
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { name, password } = this.state;
+    axios.post('http://localhost:3001/auth', { name, password })
+      .then((result) => {
+        
+      });
+      console.log(this.state);
   }
 
   render() {
     return (
-      <form className="login-form" onSubmit={this.handleSubmit}>
+      <form className="login-form" onSubmit={this.onSubmit}>
         <h2 className="login-title">Login to Seaspan</h2>
         <div className="login-input">
           <label htmlFor="username">Username: </label>
-          <input id="username" name="username" type="text" />
+          <input id="username" name="name" type="text" value={this.name} onChange={this.onChange} />
         </div>
         <div className="login-input">
           <label htmlFor="password">Password: </label>
-          <input id="password" name="password" type="password" />
+          <input id="password" name="password" type="password" value={this.password} onChange={this.onChange} />
         </div>
-        <button className="primary-btn">Send it!</button>
-        <span className="forgot-password">Forgot password? <a href="#">We can help</a></span>
+        <button className="primary-btn" type="submit">Send it!</button>
+        <span className="forgot-password">Forgot password? <a href="https://reactjs.org/">We can help</a></span>
       </form>
     );
   }
